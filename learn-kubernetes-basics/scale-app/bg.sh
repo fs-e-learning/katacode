9a51b54f47bd410651a8a7e6a1b8867e43d817d2
fs-e-learning <y.suzuki.sra@gmail.com>
2019-06-15 00:19:20 +0900

    up

--- learn-kubernetes-basics/scale-app/bg.sh
+++ learn-kubernetes-basics/scale-app/bg.sh
@@ -1,3 +1,4 @@
 minikube start
 kubectl run kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1 --port=8080
+kubectl expose deployment/kubernetes-bootcamp --type="NodePort" --port 8080
 echo "done" >> /root/katacoda-finished
